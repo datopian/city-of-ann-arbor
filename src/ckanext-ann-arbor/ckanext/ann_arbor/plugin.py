9f3d01bcd155bea4f4e17d6b82b27bb7c04d66e4
Luccas Mateus <luccasmmg@gmail.com>
2025-06-04 20:49:32 -0300

    Dashboard thumbnails

---
+++ src/ckanext-ann-arbor/ckanext/ann_arbor/plugin.py
@@ -0,0 +1,23 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+from ckanext.ann_arbor.logic.action.create import package_create as ann_arbor_create_package
+from ckanext.ann_arbor.logic.action.update import package_update as ann_arbor_update_package
+
+
+class AnnArborPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
+    
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("assets", "ann_arbor")
+
+    def get_actions(self):
+        return {'package_create': ann_arbor_create_package, 
+                'package_update': ann_arbor_update_package}
+
+    
