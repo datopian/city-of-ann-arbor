ae7def2b5e2261618e3645601a6c2e7f9ca13800
Demenech <joaommdemenech@gmail.com>
2025-06-18 13:36:54 -0300

    feat(homepage): add fallback image for the dashboard card

--- frontend/components/home/PopularDashboardCard.tsx
+++ frontend/components/home/PopularDashboardCard.tsx
@@ -3,31 +3,41 @@ import { ArrowSmallRightIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 
 export function PopularDashboardCard({ dashboard }: { dashboard: Dashboard }) {
-    console.log(dashboard);
-    return (
-        <div className="border-[2px] border-[#D9D9D9] p-3 rounded-[5px] space-y-10 bg-white">
-            <div className="aspect-video relative rounded-[5px] overflow-hidden">
-                <Image
-                    src={dashboard.dashboard_thumbnail ?? "/images/dashboard-mock-1.png"}
-                    alt="Dashboard 1"
-                    fill={true}
-                    className={"object-cover"}
-                />
-            </div>
+  return (
+    <div className="border-[2px] border-[#D9D9D9] p-3 rounded-[5px] space-y-10 bg-white">
+      <div className="aspect-video relative rounded-[5px] overflow-hidden">
+        <Image
+          src={
+            dashboard.dashboard_thumbnail ?? "/images/dashboard-fallback.jpg"
+          }
+          alt="Dashboard 1"
+          fill={true}
+          className={"object-cover"}
+        />
+      </div>
 
-            <div className="space-y-3 px-2">
-                <h3 className="font-bold text-3xl line-clamp-2 h-[4.5rem]">{dashboard.title}</h3>
-                <p className="text-[#6E6E6E] text-sm leading-[20px] line-clamp-4 text-ellipsis h-[5.5em]">
-                    {dashboard.notes}
-                </p>
-            </div>
+      <div className="space-y-3 px-2">
+        <h3 className="font-bold text-3xl line-clamp-2 h-[4.5rem]">
+          {dashboard.title}
+        </h3>
+        <p className="text-[#6E6E6E] text-sm leading-[20px] line-clamp-4 text-ellipsis h-[5.5em]">
+          {dashboard.notes ? (
+            dashboard.notes
+          ) : (
+            <span className="italic">N/A</span>
+          )}
+        </p>
+      </div>
 
-            <div className="px-2 pb-5">
-                <button className="bg-ann-arbor-accent-green transition-all hover:bg-opacity-90 text-white font-bold px-3 py-2 rounded-[5px] flex items-center">
-                    Explore dashboard
-                    <ArrowSmallRightIcon className="inline w-6" />
-                </button>
-            </div>
-        </div>
-    );
+      <div className="px-2 pb-5">
+        <a
+          href={`#dashboard-${dashboard.name}`}
+          className="w-fit bg-ann-arbor-accent-green transition-all hover:bg-opacity-90 text-white font-bold px-3 py-2 rounded-[5px] flex items-center"
+        >
+          Explore dashboard
+          <ArrowSmallRightIcon className="inline w-6" />
+        </a>
+      </div>
+    </div>
+  );
 }
