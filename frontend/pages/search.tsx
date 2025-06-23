252522a50a41fe46fee7810e2b67191c256cce5b
Luccas Mateus <luccasmmg@gmail.com>
2025-06-23 12:10:00 -0300

    Search page

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -25,6 +25,14 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import Image from "next/image";
 import {
   AdjustmentsHorizontalIcon,
@@ -32,10 +40,14 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 
-export async function getServerSideProps() {
+const ITEMS_PER_PAGE = 5;
+
+export async function getServerSideProps({ query }) {
+  const q = query?.q;
   const initialRequestOption: PackageSearchOptions = {
+    query: q ?? "",
     offset: 0,
-    limit: 10,
+    limit: ITEMS_PER_PAGE,
     tags: [],
     groups: [],
     orgs: [],
@@ -53,12 +65,13 @@ export async function getServerSideProps() {
       searchFacets: {
         ...search_result.search_facets,
       },
+      query: initialRequestOption.query,
     },
   };
 }
 
-export function SearchHero() {
-  const [searchQuery, setSearchQuery] = useState("");
+export function SearchHero({ query }: { query: string }) {
+  const [searchQuery, setSearchQuery] = useState(query);
   const { setValue } = useFormContext();
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     if (e) {
@@ -72,7 +85,7 @@ export function SearchHero() {
       <div className="mx-auto">
         <div className="flex flex-col lg:items-center gap-y-7 relative h-fit">
           <div className="z-10 max-w-[1128px] mx-auto w-full">
-            <h1 className="lg:max-w-[478px] font-bold text-3xl mx-auto text-[40px] lg:text-6xl flex flex-col text-center !leading-snug pb-[120px]">
+            <h1 className="lg:max-w-[478px] font-bold text-3xl mx-auto text-[40px] lg:text-6xl flex flex-col text-center !leading-snug pb-8 lg:pb-[120px]">
               Search Data
             </h1>
             <div className="w-full p-4 bg-white rounded-lg">
@@ -85,6 +98,7 @@ export function SearchHero() {
                     id="search-form-input"
                     type="search"
                     name="search"
+                    defaultValue={query}
                     onChange={(e) => {
                       setSearchQuery(e.target.value);
                     }}
@@ -123,10 +137,12 @@ interface SearchFormData {
 export default function DatasetSearch({
   fallback,
   searchFacets,
+  query,
 }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const form = useForm<SearchFormData>({
     defaultValues: {
-      query: "",
+      query,
       groups: [],
       orgs: [],
       resFormat: [],
@@ -153,7 +169,6 @@ export default function DatasetSearch({
   const topics = searchFacets?.groups?.items || [];
   const resFormat = searchFacets?.res_format?.items || [];
   const tags = searchFacets?.tags?.items || [];
-  const ITEMS_PER_PAGE = 5;
   const totalPages = data?.count ? Math.ceil(data.count / ITEMS_PER_PAGE) : 1;
 
   const activeFilters = useMemo(() => {
@@ -204,6 +219,151 @@ export default function DatasetSearch({
     setValue("tags", []);
     setValue("type", []);
   };
+
+  const FilterSidebar = ({ className = "" }: { className?: string }) => (
+    <div
+      className={`bg-white rounded-lg p-6 pt-4 pl-0 max-h-[calc(100vh-8.5rem)] overflow-y-auto ${className}`}
+    >
+      <div className="flex items-center gap-2 mb-4 hidden lg:block">
+        <AdjustmentsHorizontalIcon className="w-5 h-5 text-gray-600" />
+        <h2 className="text-xl font-medium text-gray-800">Filters</h2>
+      </div>
+      {activeFilters.length > 0 && (
+        <div className="mb-6">
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="font-normal text-lg text-gray-800">
+              Active filters
+            </h3>
+            <button
+              onClick={clearAllFilters}
+              className="text-sm text-gray-800 hover:text-teal-800 font-medium flex items-center gap-1"
+            >
+              Clear all
+              <XMarkIcon className="w-3 h-3" />
+            </button>
+          </div>
+          <div className="flex flex-wrap gap-2 mt-2">
+            {activeFilters.map((filter) => (
+              <Badge
+                key={filter.label}
+                variant="secondary"
+                className="text-white py-1 px-2 text-base font-normal bg-[#5e98a4] border-0"
+              >
+                {filter.label}
+                <button
+                  onClick={() =>
+                    removeFilter(
+                      filter.type as keyof SearchFormData,
+                      filter.value
+                    )
+                  }
+                  className="ml-1.5"
+                >
+                  <X className="w-3 h-3" />
+                </button>
+              </Badge>
+            ))}
+          </div>
+        </div>
+      )}
+      <Accordion type="multiple" className="w-full">
+        {[
+          {
+            title: "Type",
+            items: [
+              {
+                name: "dataset",
+                display_name: "Dataset",
+              },
+              {
+                name: "dashboard",
+                display_name: "Dashboard",
+              },
+            ],
+            formName: "type" as const,
+            value: "type",
+          },
+          {
+            title: `Topics ${
+              formData.groups.length > 0 ? `(${formData.groups.length})` : ""
+            }`,
+            items: topics,
+            formName: "groups" as const,
+            value: "topics",
+          },
+          {
+            title: "Formats",
+            items: resFormat,
+            formName: "resFormat" as const,
+            value: "resFormat",
+          },
+          {
+            title: "Tags",
+            items: tags,
+            formName: "tags" as const,
+            value: "tags",
+          },
+        ]
+          .filter((i) => i.items.length > 0)
+          .map((filterGroup) => (
+            <AccordionItem
+              key={filterGroup.value}
+              value={filterGroup.value}
+              className="border-none"
+            >
+              <AccordionTrigger className="font-normal text-gray-700 hover:text-teal-600 hover:no-underline py-4 text-lg">
+                <div className="flex items-center gap-2">
+                  {filterGroup.title}
+                  {formData[filterGroup.formName].length > 0 && (
+                    <div className="w-5 h-4 relative rounded-full">
+                      <div className="w-5 h-4 left-0 top-0 absolute bg-neutral-200 rounded-full" />
+                      <div className="w-[5px] h-2.5 left-[7px] top-[0px] absolute justify-start text-[#313131] text-xs font-medium font-['Fira_Sans']">
+                        {formData[filterGroup.formName].length}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="space-y-2.5 pb-4">
+                {filterGroup.items
+                  .filter((i) => i.name)
+                  .map((item) => (
+                    <Controller
+                      key={item.name}
+                      name={filterGroup.formName}
+                      control={control}
+                      render={({ field }) => (
+                        <div className="flex items-center space-x-2.5">
+                          <Checkbox
+                            id={`${filterGroup.formName}-${item.name}`}
+                            checked={field.value.includes(item.name)}
+                            onCheckedChange={(checked) => {
+                              const newValue = checked
+                                ? [...field.value, item.name]
+                                : field.value.filter(
+                                    (v: string) => v !== item.name
+                                  );
+                              field.onChange(newValue);
+                              setValue("offset", 0);
+                            }}
+                            className="data-[state=checked]:bg-teal-600 border-2 border-gray-300 bg-gray-300 data-[state=checked]:border-teal-600 focus:ring-teal-500"
+                          />
+                          <label
+                            htmlFor={`${filterGroup.formName}-${item.name}`}
+                            className="text-base font-normal text-gray-600 hover:text-gray-900 cursor-pointer flex-grow"
+                          >
+                            {item.display_name ? item.display_name : item.name}
+                          </label>
+                        </div>
+                      )}
+                    />
+                  ))}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+      </Accordion>
+    </div>
+  );
 
   const renderPagination = () => {
     if (!data || totalPages <= 1) return null;
@@ -292,168 +452,18 @@ export default function DatasetSearch({
       <FormProvider {...form}>
         <div className="lg:min-h-screen bg-gradient-to-b from-[#E2F1E4] to-[#FFFFFF] to-10%">
           <NavBar />
-          <SearchHero />
-          <div className="space-y-2 mt-4">
+          <SearchHero query={query} />
+          <div className="space-y-2 lg:mt-4">
             {/* Main Content */}
             <div className="max-w-[1128px] mx-auto px-4 pb-8 flex-grow w-full">
               <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
-                {/* Sidebar Filters */}
-                <aside className="lg:col-span-1 lg:sticky lg:top-28 self-start">
-                  {" "}
-                  {/* Sticky position with top offset */}
-                  <div className="bg-white rounded-lg p-6 pt-4 pl-0 max-h-[calc(100vh-8.5rem)] overflow-y-auto">
-                    {" "}
-                    {/* Scrollable inner content */}
-                    <div className="flex items-center gap-2 mb-4">
-                      <AdjustmentsHorizontalIcon className="w-5 h-5 text-gray-600" />
-                      <h2 className="text-xl font-medium text-gray-800">
-                        Filters
-                      </h2>
-                    </div>
-                    {activeFilters.length > 0 && (
-                      <div className="mb-6">
-                        <div className="flex items-center justify-between mb-2">
-                          <h3 className="font-normal text-lg text-gray-800">
-                            Active filters
-                          </h3>
-                          <button
-                            onClick={clearAllFilters}
-                            className="text-sm text-gray-800 hover:text-teal-800 font-medium flex items-center gap-1"
-                          >
-                            Clear all
-                            <XMarkIcon className="w-3 h-3" />
-                          </button>
-                        </div>
-                        <div className="flex flex-wrap gap-2 mt-2">
-                          {activeFilters.map((filter) => (
-                            <Badge
-                              key={filter.label}
-                              variant="secondary"
-                              className="text-white py-1 px-2 text-base font-normal bg-[#5e98a4] border-0"
-                            >
-                              {filter.label}
-                              <button
-                                onClick={() =>
-                                  removeFilter(
-                                    filter.type as keyof SearchFormData,
-                                    filter.value
-                                  )
-                                }
-                                className="ml-1.5"
-                              >
-                                <X className="w-3 h-3" />
-                              </button>
-                            </Badge>
-                          ))}
-                        </div>
-                      </div>
-                    )}
-                    <Accordion type="multiple" className="w-full">
-                      {[
-                        {
-                          title: "Type",
-                          items: [
-                            {
-                              name: "dataset",
-                              display_name: "Dataset",
-                            },
-                            {
-                              name: "dashboard",
-                              display_name: "Dashboard",
-                            },
-                          ],
-                          formName: "type" as const,
-                          value: "type",
-                        },
-                        {
-                          title: `Topics ${formData.groups.length > 0
-                              ? `(${formData.groups.length})`
-                              : ""
-                            }`,
-                          items: topics,
-                          formName: "groups" as const,
-                          value: "topics",
-                        },
-                        {
-                          title: "Formats",
-                          items: resFormat,
-                          formName: "resFormat" as const,
-                          value: "resFormat",
-                        },
-                        {
-                          title: "Tags",
-                          items: tags,
-                          formName: "tags" as const,
-                          value: "tags",
-                        },
-                      ]
-                        .filter((i) => i.items.length > 0)
-                        .map((filterGroup) => (
-                          <AccordionItem
-                            key={filterGroup.value}
-                            value={filterGroup.value}
-                            className="border-none"
-                          >
-                            <AccordionTrigger className="font-normal text-gray-700 hover:text-teal-600 hover:no-underline py-4 text-lg">
-                              <div className="flex items-center gap-2">
-                                {filterGroup.title}
-                                {formData[filterGroup.formName].length > 0 && (
-                                  <div className="w-5 h-4 relative rounded-full">
-                                    <div className="w-5 h-4 left-0 top-0 absolute bg-neutral-200 rounded-full" />
-                                    <div className="w-[5px] h-2.5 left-[7px] top-[0px] absolute justify-start text-[#313131] text-xs font-medium font-['Fira_Sans']">
-                                      {formData[filterGroup.formName].length}
-                                    </div>
-                                  </div>
-                                )}
-                              </div>
-                            </AccordionTrigger>
-                            <AccordionContent className="space-y-2.5 pb-4">
-                              {filterGroup.items
-                                .filter((i) => i.name)
-                                .map((item) => (
-                                  <Controller
-                                    key={item.name}
-                                    name={filterGroup.formName}
-                                    control={control}
-                                    render={({ field }) => (
-                                      <div className="flex items-center space-x-2.5">
-                                        <Checkbox
-                                          id={`${filterGroup.formName}-${item.name}`}
-                                          checked={field.value.includes(
-                                            item.name
-                                          )}
-                                          onCheckedChange={(checked) => {
-                                            const newValue = checked
-                                              ? [...field.value, item.name]
-                                              : field.value.filter(
-                                                (v: string) => v !== item.name
-                                              );
-                                            field.onChange(newValue);
-                                            setValue("offset", 0);
-                                          }}
-                                          className="data-[state=checked]:bg-teal-600 border-2 border-gray-300 bg-gray-300 data-[state=checked]:border-teal-600 focus:ring-teal-500"
-                                        />
-                                        <label
-                                          htmlFor={`${filterGroup.formName}-${item.name}`}
-                                          className="text-base font-normal text-gray-600 hover:text-gray-900 cursor-pointer flex-grow"
-                                        >
-                                          {item.display_name
-                                            ? item.display_name
-                                            : item.name}
-                                        </label>
-                                      </div>
-                                    )}
-                                  />
-                                ))}
-                            </AccordionContent>
-                          </AccordionItem>
-                        ))}
-                    </Accordion>
-                  </div>
+                {/* Desktop Sidebar Filters */}
+                <aside className="hidden lg:block lg:col-span-1 lg:sticky lg:top-28 self-start">
+                  <FilterSidebar />
                 </aside>
 
                 {/* Results */}
-                <main className="lg:col-span-3 pt-4">
+                <main className="col-span-1 lg:col-span-3 pt-4">
                   <div className="mb-4 flex items-center justify-between">
                     <h2 className="pl-4 sm:pl-6 text-xl font-medium text-gray-800">
                       {isLoading
@@ -516,6 +526,26 @@ export default function DatasetSearch({
             </div>
             <Footer />
           </div>
+        </div>
+
+        {/* Mobile Filter Drawer */}
+        <div className="lg:hidden">
+          <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+            <DrawerTrigger asChild>
+              <Button className="fixed bottom-4 left-4 z-50 bg-ann-arbor-accent-green hover:bg-ann-arbor-accent-green/90 text-white shadow-lg">
+                <AdjustmentsHorizontalIcon className="w-5 h-5 mr-2" />
+                Filter data
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent className="max-h-[85vh]">
+              <DrawerHeader>
+                <DrawerTitle>Filter Data</DrawerTitle>
+              </DrawerHeader>
+              <div className="px-4 pb-4 overflow-y-auto">
+                <FilterSidebar className="p-0" />
+              </div>
+            </DrawerContent>
+          </Drawer>
         </div>
       </FormProvider>
     </div>
