edfae8fb49a611e7170b26057472bb1a22e34710
Luccas Mateus <luccasmmg@gmail.com>
2025-06-23 13:22:09 -0300

    Fix z-index

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -532,7 +532,7 @@ export default function DatasetSearch({
         <div className="lg:hidden">
           <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
             <DrawerTrigger asChild>
-              <Button className="fixed bottom-4 left-4 z-50 bg-ann-arbor-accent-green hover:bg-ann-arbor-accent-green/90 text-white shadow-lg">
+              <Button className="fixed bottom-4 left-4 z-20 bg-ann-arbor-accent-green hover:bg-ann-arbor-accent-green/90 text-white shadow-lg">
                 <AdjustmentsHorizontalIcon className="w-5 h-5 mr-2" />
                 Filter data
               </Button>
