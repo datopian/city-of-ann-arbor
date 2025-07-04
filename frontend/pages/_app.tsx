da0426f9e95c487cfb10bc250afb80bfc5bce6ac
Demenech <joaommdemenech@gmail.com>
2025-07-04 16:10:31 -0300

    feat(ga): implement google analytics

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,18 +5,34 @@ import "@/styles/tabs.scss";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 import SEO from "../next-seo.config";
-
 import Loader from "../components/_shared/Loader";
+import { Fira_Sans } from "next/font/google";
+import { useRouter } from "next/router";
 
-import { Fira_Sans } from 'next/font/google'
+const FiraSans = Fira_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID ?? "";
 
-const FiraSans = Fira_Sans({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] })
+const handleRouteChange = (url: URL) => {
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <QueryClientProvider client={queryClient}>
