06161777980dd07579a4deb3e7b16670c2e10e6a
Demenech <joaommdemenech@gmail.com>
2025-10-03 09:16:52 -0300

    fix(authz): allow anon users to call group_show

--- src/ckanext-ann-arbor/ckanext/ann_arbor/logic/auth.py
+++ src/ckanext-ann-arbor/ckanext/ann_arbor/logic/auth.py
@@ -1,6 +1,9 @@
 import logging
+import ckan.plugins as p
 
 log = logging.getLogger(__name__)
 
+
+@p.toolkit.auth_allow_anonymous_access
 def group_show(context, data_dict):
     return {"success": True}
